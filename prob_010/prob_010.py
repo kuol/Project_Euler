d0a3a82ca24fa34d2f78b19935f11208ccac92b1
Kuo Liu <liukuo99@gmail.com>
2014-11-28 00:04:11 -0700

    modified sieve algo

--- prob_010/prob_010.py
+++ prob_010/prob_010.py
@@ -1,12 +1,11 @@
 import timeit
 def main():
-#    start = timeit.default_timer()
-#    a = prime(2000000)
-#    end = timeit.default_timer()
-#    print sum(a)
-#    print "Time is: %f" %(end-start)
+    start = timeit.default_timer()
+    a = sieve2_prime(2000000)
+    end = timeit.default_timer()
+    print a
+    print "Time is: %f" %(end-start)
 
-    print sieve_prime(2000000)
 
 def brute_prime(N):
     """ Find primes numbers that are less than N
@@ -21,6 +20,7 @@ def brute_prime(N):
             p.append(i)
     return p
 
+# Time is: 1.107616
 def sieve_prime(N):
     a = range(2,N+1)
     p = 2
@@ -39,6 +39,50 @@ def sieve_prime(N):
         p += 1
     return sum(a)
 
+# Time is: 0.884902
+# Modified Sieve, only check numbers > p*p
+def sieve1_prime(N):
+    a = range(2,N+1)
+    p = 2
+    while p <= N:
+        # sieve
+        if p <= N/p:
+            for i in xrange(p, N/p+1):
+                if a[i*p-2]:
+                    a[i*p-2] = 0
+        # move to the next prime
+        while a[p-1] == 0:
+            if p+1 >= N:
+                return sum(a)
+            else:
+                p += 1
+        p += 1
+    return sum(a)
+
+# Time is: 0.577991
+# Modified Sieve, 
+#    * only check numbers > p*p
+#    * only check odd numbers
+def sieve2_prime(N):
+    a = [2] + range(3,N+1,2)
+    p = 3 
+    while p <= N:
+        # sieve
+        if p <= N/p:
+            for i in xrange(p, N/p+1, 2):
+                ind = (i*p-1)/2
+                if a[ind]:
+                    a[ind] = 0
+        # move to the next prime
+        ind = (p-1)/2+1
+        while a[ind] == 0:
+            if (p+2 > N-1 and N%2 == 1) or (p+2 > N-2 and N%2 == 0):
+                return sum(a)
+            else:
+                p += 2
+                ind = (p-1)/2 + 1
+        p += 2
+    return sum(a)
 
 if __name__ == '__main__':
     main()
